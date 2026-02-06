a89690639f502d928de4986d96f966f4bac79aa5
Shawn Miller <shawn@mediocre.com>
2026-02-05 23:35:58 -0600

    Update NVM installation script version to 0.40.4

--- init.sh
+++ init.sh
@@ -16,7 +16,7 @@ brew install redis
 brew services start redis
 
 # NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
